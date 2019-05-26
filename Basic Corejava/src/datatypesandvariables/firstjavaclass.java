bfa00ba28a4bc67684879c6e877548763dc806fe
kavishwaramruta <kavishwaramruta@gmail.com>
2019-05-26 04:35:26 -0700

    committing first java class file

---
+++ Basic Corejava/src/datatypesandvariables/firstjavaclass.java
@@ -0,0 +1,10 @@
+package datatypesandvariables;
+
+public class firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to the world of automation");
+	}
+
+}
